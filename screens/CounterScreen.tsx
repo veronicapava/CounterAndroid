d2f6d5cdb73ecee2829b4f022b54dc4875688ff6
veronicapava <veronicapava@gmail.com>
2023-07-24 15:37:07 -0500

    Crea pantalla principal y se integra el boton

--- screens/CounterScreen.tsx
+++ screens/CounterScreen.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import {Text, TouchableOpacity, View, StyleSheet } from 'react-native'
+import Fab from '../buttons/Fab'
 
 const CounterScreen = () => {
     const [count, setCount] = useState(10)
@@ -9,17 +10,16 @@ const CounterScreen = () => {
     <View style={style.container}>
         <Text style={style.counter}>Counter: {count}</Text>
 
-        <TouchableOpacity onPress={() => setCount(count + 1)} style={style.fabLocationBR}>
-            <View style={style.fab}>
-                <Text style={style.fabText}>+1</Text>
-            </View>
-        </TouchableOpacity>
+        <Fab
+            title="+1"
+            onPress={() => setCount(count + 1)}
+        />
 
-        <TouchableOpacity onPress={() => setCount(count - 1)} style={style.fabLocationBL}>
-            <View style={style.fab}>
-                <Text style={style.fabText}>-1</Text>
-            </View>
-        </TouchableOpacity>
+        <Fab
+            title="-1"
+            onPress={() => setCount(count - 1)}
+            position="bl"
+        />
     </View>
   )
 }
@@ -29,8 +29,4 @@ export default CounterScreen
 const style = StyleSheet.create({
     container: {flex:1, backgroundColor:"pink",justifyContent: "center"},
     counter: {fontSize:50, textAlign:"center"},
-    fab: {backgroundColor:"red", width:60, height:60,borderRadius:100},
-    fabLocationBR: {position: "absolute",bottom:10, right:10},
-    fabLocationBL: {position: "absolute",bottom:10, left:10},
-    fabText: { alignSelf:'center',color: "white", fontSize:25, fontWeight:"bold" }
 })
